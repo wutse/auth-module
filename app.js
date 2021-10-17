f9424e94c26ec42852cc0a843cb3ec07b56d0b6e
foxwuys <foxwuys@gmail.com>
2021-10-17 22:29:42 +0800

    add session module : express-session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const routers = require('./routes/index');
+const session = require('express-session');
 
 require('./config/mongoose');
 
@@ -13,6 +14,12 @@ const port = 3000;
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
+app.use(session({
+  secret: 'ac exercise',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,6 +3,8 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
+  console.log(req.session);
+  console.log(req.sessionID);
   res.render('home', { userName: req.query.name });
 });
 
